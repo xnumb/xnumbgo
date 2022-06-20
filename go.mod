3a2904aa759de0e7c473b48057dbbf2b2a8ddfdb
xnumb <xnumbs@gmail.com>
2022-06-20 11:39:05 +0800

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module xnumbgo
+
+go 1.18
+
+require github.com/gogf/gf/v2 v2.0.6
+
+require (
+	go.opentelemetry.io/otel v1.0.0 // indirect
+	go.opentelemetry.io/otel/trace v1.0.0 // indirect
+)
---
+++ uuid/uuid.go
@@ -0,0 +1,17 @@
+package util
+
+import "github.com/gogf/gf/v2/util/guid"
+
+func S(s ...string) string {
+	r := ""
+	if len(s) == 0 {
+		r = guid.S()
+	}
+	if len(s) == 1 {
+		r = guid.S([]byte(s[0]))
+	}
+	if len(s) == 2 {
+		r = guid.S([]byte(s[0]), []byte(s[1]))
+	}
+	return r
+}
